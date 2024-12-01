17b75538d051a7ac1da31ce543778bf301073434
rp-honorato <rpedro.honorato@gmail.com>
2024-12-01 18:35:01 +0000

    IDKWIFD (kkkk)

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-import {} from 'node:fs'
+import { createServer } from 'node:http'
+
+const server = createServer((request, response) => {
+    response.write('Oi!')
+
+    return response.end();
+})
+
+server.listen(3333)
+
+//localhost:3333
